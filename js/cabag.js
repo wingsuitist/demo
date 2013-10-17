86f93a2f5d74b29f8397ab1b1b3f5a58328e361d
rbloechlinger <rb@ws23.cabnet.cabag.ch>
2013-10-17 10:57:18 +0200

    update

--- js/cabag.js
+++ js/cabag.js
@@ -1,17 +1,39 @@
 function lookup_location() {
-  geoPosition.getCurrentPosition(showGeo, showGeoError);
+	if (geoPosition.init()) {
+		geoPosition.getCurrentPosition(showGeo, showGeoError);
+  	}
 }
 
 function showGeo(loc) {
-  $("#geo-wrapper").css({'width':'320px','height':'350px'});
-  var map = new GMap2(document.getElementById("geo-wrapper"));
-  var center = new GLatLng(loc.coords.latitude, loc.coords.longitude);
-  map.setCenter(center, 14);
-  map.addControl(new GSmallMapControl());
-  map.addControl(new GMapTypeControl());
-  map.addOverlay(new GMarker(center, {draggable: false, title: "You are here (more or less)"}));
+  $("#geo").html("lat: " + loc.coords.latitude + "<br />long: " + loc.coords.longitude);
 }
 
 function showGeoError() {
-  $("#live-geolocation").html('Unable to determine your location.');
+  $("#geo").html('Unable to determine your location.');
 }
+
+function tilt(x,y) {
+	x = (x == null) ? 0 : Math.floor(x);
+	y = (y == null) ? 0 : Math.floor(y);
+	$("#gyro").html("x: " + x + "<br />y: " + y);
+	var rgb = "rgb(" + (120 + (x * 10)) + "," + (120 + (y * 10)) + ",0)";
+	//console.log("rgb: "+rgb);
+	$("#gyro").css({"background-color":rgb});
+}
+
+$( document ).ready(function() {
+
+	$("#geo").hide();
+	$("#gyro").hide();
+	
+	lookup_location();
+	tilt();
+	
+	$("#navigation li a").click(function() {
+			//console.log("id: "+$(this).attr("href"));
+			var id = $(this).attr("href");
+			$(".content_container").hide();
+			$(id).show();			
+	});
+
+});
--- js/gyro.js
+++ js/gyro.js
@@ -1,13 +1,13 @@
 if (window.DeviceOrientationEvent) {
     window.addEventListener("deviceorientation", function () {
-        tilt([event.beta, event.gamma]);
+        tilt(event.beta, event.gamma);
     }, true);
 } else if (window.DeviceMotionEvent) {
     window.addEventListener('devicemotion', function () {
-        tilt([event.acceleration.x * 2, event.acceleration.y * 2]);
+        tilt(event.acceleration.x * 2, event.acceleration.y * 2);
     }, true);
 } else {
     window.addEventListener("MozOrientation", function () {
-        tilt([orientation.x * 50, orientation.y * 50]);
+        tilt(orientation.x * 50, orientation.y * 50);
     }, true);
 }
